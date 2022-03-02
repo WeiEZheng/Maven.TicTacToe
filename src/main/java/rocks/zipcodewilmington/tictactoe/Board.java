95d46ae220d8458df16509d2417e599678817302
Wei <wei@Zipcoders-MacBook-Pro.local>
2022-03-02 11:33:52 -0500

    added some win conditions

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,7 +4,10 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    private Character[][] boardState;
+
     public Board(Character[][] matrix) {
+        boardState = matrix;
     }
 
     public Boolean isInFavorOfX() {return getWinner().equalsIgnoreCase("X");}
@@ -18,7 +21,21 @@ public class Board {
     }
 
     public String getWinner() {
-        return null;
+        String winner="";
+        if ((boardState[0][0] == boardState[1][1]) &&
+                (boardState[0][0] == boardState[2][2])) {
+            winner = boardState[0][0].toString();
+        }
+        else if ((boardState[3][1] == boardState[1][3]) &&
+                (boardState[3][1] == boardState[2][2])) {
+            winner = boardState[3][1].toString();
+        }
+        else{
+            for (int i=0;i>3;i++) {
+
+            }
+        }
+        return winner;
     }
 
 }
